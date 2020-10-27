b95a92a28e51db419ea62941fe741d0e711362c3
mherzog01 <mherzog01@gmail.com>
2020-10-27 15:01:00 -0400

    Added mk_bucket_dirs.sh

---
+++ sh/mk_bucket_dirs.sh
@@ -0,0 +1,29 @@
+# 1.  Mount $BUCKET_NAME at $MOUNT_PT
+# 2.  Run this script
+MOUNT_PT=$HOME/mnt
+BUCKET_NAME=desired_bucket
+DEL_OUTFILE=n    # Set to y or n
+
+echo "Reading objects in $BUCKET_NAME"
+OUTFILE=dir_names.txt
+gsutil ls -r gs://$BUCKET_NAME/** | while read BUCKET_OBJ
+do   
+    dirname "$BUCKET_OBJ"
+done | sort -u > $OUTFILE
+echo "Processing directories found"
+cat $OUTFILE | while read DIR_NAME
+do
+    LOCAL_DIR=`echo "$DIR_NAME" | sed "s=gs://$BUCKET_NAME/==" | sed "s=gs://$BUCKET_NAME=="`
+    #echo $LOCAL_DIR
+    TARG_DIR="$MOUNT_PT/$LOCAL_DIR"
+    if ! [ -d "$TARG_DIR" ]
+    then
+	    echo "Creating $TARG_DIR"
+        mkdir -p "$TARG_DIR"
+    fi
+done
+if [ $DEL_OUTFILE = "y" ]
+then
+    rm $OUTFILE
+fi
+echo "Process complete"
